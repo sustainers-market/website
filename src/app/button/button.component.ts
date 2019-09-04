336114bfd7277d44caa15b92470d52774b5be1d1
Tyler Hayes <tylerxhayes@gmail.com>
2019-09-03 17:59:14 -0700

    wip

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,11 +1,23 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
-@Component({
+@Component( {
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
-})
+  styleUrls: [ './button.component.scss' ]
+} )
 export class ButtonComponent {
   @Input() text: string
-  @Input() action: void
+  @Input() action: Function
+  @Input() link: string
+
+  constructor(private router: Router) {}
+
+  onClick() {
+    if (this.link) {
+      this.router.navigate([this.link])
+    } else if (this.action) {
+      this.action()
+    }
+  }
 }
